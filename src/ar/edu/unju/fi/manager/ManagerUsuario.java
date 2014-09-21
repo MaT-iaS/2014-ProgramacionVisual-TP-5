ca69747a4997ca4bf453c505af12dd912146cffe
MaTiaS <mts1788@gmail.com>
2014-09-21 01:11:40 -0300

    prueba Primer commit

---
+++ src/ar/edu/unju/fi/manager/ManagerUsuario.java
@@ -0,0 +1,24 @@
+package ar.edu.unju.fi.manager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.ejemplo.dominio.Rol;
+import com.ejemplo.dominio.Usuario;
+
+public class ManagerUsuario {
+	public static List<Usuario> usuarios;
+	static{
+		if (usuarios == null){
+			Rol vendedor = new Rol(0, "VENDEDOR");
+			Rol administrador = new Rol(1, "ADMINISTRADOR");
+			usuarios = new ArrayList<Usuario>();
+			usuarios.add(new Usuario(1111, "Maria", "Tinte", "1111", "ACTIVO", vendedor));
+			usuarios.add(new Usuario(2222, "Liliana", "Cruz", "2222","NO ACTIVO", vendedor));
+			usuarios.add(new Usuario(3333, "Pedro", "Sajama", "3333", "ACTIVO", vendedor));
+			usuarios.add(new Usuario(4444, "Carlos", "Mamani", "4444", "NO ACTIVO", administrador));
+			usuarios.add(new Usuario(5555, "Rosana", "Mendez", "5555", "ACTIVO", administrador));
+		}
+	}
+
+}
